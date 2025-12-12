7bab1db39fd1c3a29fdfd678969311861aaf2e4f
eliseu <eliseu@eliseu.eng.br>
2025-12-12 12:17:38 -0300

    Revert "melhoria nos templates da grid, trocando let-raw por let-data"
    This reverts commit 1a2c12c80205abe4d1b6fb8e20897f822bdf322a.

--- front/src/app/shared/model/evento.ts
+++ front/src/app/shared/model/evento.ts
@@ -1,23 +1,22 @@
-import {Cliente} from '@/shared/model/cliente';
-import {EventoPessoa} from '@/shared/model/evento-pessoa';
-import {EventoProduto} from '@/shared/model/evento-produto';
+import { Cliente } from '@/shared/model/cliente';
+import { EventoPessoa } from '@/shared/model/evento-pessoa';
+import { EventoProduto } from '@/shared/model/evento-produto';
 import {StatusEnum} from "@/shared/model/enum/status.enum";
 
 export class Evento {
+  id?: number;
+  nome?: string;
+  descricao?: string;
+  cliente?: Cliente | { id: number };
+  status?: StatusEnum;
+  anotacoes?: string;
+  inicio?: string | Date;
+  fimPrevisto?: string | Date;
+  fim?: string | Date;
 
-    id?: number;
-    nome?: string;
-    descricao?: string;
-    cliente?: Cliente | { id: number };
-    status?: StatusEnum;
-    anotacoes?: string;
-    inicio?: string | Date;
-    fimPrevisto?: string | Date;
-    fim?: string | Date;
+  pessoas?: EventoPessoa[];
+  produtos?: EventoProduto[];
 
-    pessoas?: EventoPessoa[];
-    produtos?: EventoProduto[];
-
-    // Concorrência otimista
-    version?: number;
+  // Concorrência otimista
+  version?: number;
 }
